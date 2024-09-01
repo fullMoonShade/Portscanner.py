8624e336fb61c8f35cee0889391992cbdbe4975e
Shei <72736290+fullMoonShade@users.noreply.github.com>
2024-09-01 21:36:35 +0600

    Update portscanner.py
    - Added threading.Event to handle KeyboardInterrupt more efficiently

--- portscanner.py
+++ portscanner.py
@@ -4,6 +4,10 @@ import socket
 from datetime import datetime
 import threading
 
+stop_event = threading.Event()
+
+
+# Banner 
 ascii_banner = pyfiglet.figlet_format("Port Scanner")
 print(ascii_banner)
 
@@ -13,17 +17,17 @@ else:
     print("Invalid argument. Please provide a valid hostname or IP address.")
     sys.exit()
 
-# Banner
 print("-" * 50)
 print("Scanning Target: " + target)
 print("Scanning started at: " + str(datetime.now()))
 print("-" * 50)
 
-
 def scan_port(port):
+    if stop_event.is_set():
+        return
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
+        socket.setdefaulttimeout(0.5)
         result = s.connect_ex((target, port))
         if result == 0:
             print(f"Port {port} is open")
@@ -31,30 +35,33 @@ def scan_port(port):
     except Exception as e:
         pass
 
-
 try:
     threads = []
     for port in range(1, 65535):
+        if stop_event.is_set():
+            break
         thread = threading.Thread(target=scan_port, args=(port,))
         threads.append(thread)
         thread.start()
 
-        
-        if len(threads) >= 100:  # Limit to 100 threads at a time
+        if len(threads) >= 200: # Adjust Threads
             for thread in threads:
                 thread.join()
             threads = []
 
-   
     for thread in threads:
         thread.join()
 
 except KeyboardInterrupt:
-    print("\n Exiting Program!!!")
+    print("\nExiting Program!!!")
+    stop_event.set()
+    for thread in threads:
+        thread.join()
     sys.exit()
+
 except socket.gaierror:
-    print("\n Hostname Could Not Be Resolved!!!")
+    print("\nHostname Could Not Be Resolved!!!")
     sys.exit()
 except socket.error:
-    print("\n Server not responding!!!")
+    print("\nServer not responding!!!")
     sys.exit()
